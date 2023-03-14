25b94c848dc2920ec3d9012785af149504eab850
tylerjyee <tyler.j.yee@sjsu.edu>
2023-03-14 12:06:43 -0700

    small edits to sort.py

--- sort.py
+++ sort.py
@@ -1,22 +1,8 @@
-"""from operator import itemgetter
-
-def sort_tup(t_dict):
-    res = sorted(t_dict.values(), key=itemgetter(1))
-    return res
-
-t_dict = {"Sam": (1234567, 20), "Peter": (5567823, 32), "Alex": (8907346, 25)}
-d = sort_tup(t_dict)
-print(d)"""
-
 from operator import itemgetter
 t_dict = {"Sam": (1234567, 20), "Peter": (5567823, 32), "Alex": (8907346, 25)}
-d = dict(t_dict)
 
 def sort_tup(t_dict):
     res = sorted(t_dict.values(), key=itemgetter(1))
-    for name, info in res.items():
-        ph, age = info
-        list_fin = [{name},{ph}]
-        return(list_fin)
 
 print(sort_tup(t_dict))
+
