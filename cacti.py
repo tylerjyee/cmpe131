e93bf173487dd832b07e9647f91b554f6c78e638
tylerjyee <tyler.j.yee@sjsu.edu>
2023-03-17 15:34:10 -0700

    Changes to cacti program

--- cacti.py
+++ cacti.py
@@ -1,26 +1,25 @@
 def cacti_number(plot):
     count = 0
-    x_lim = (0, len(plot[0])-1)
-    y_lim = (0, len(plot)-1)
+    x_bounds = (0, len(plot[0])-1)
+    y_bounds = (0, len(plot)-1)
 
     for x in range(len(plot[0])):
         for y in range(len(plot)):
-
             if plot[y][x] == 0:
                 step = 1
-                if not (y_lim[0] > x + 1 or y_lim[1] < x + 1):
-                    if plot[y][x + 1] != 0:
+                if not (x_bounds[0] > x+1 or x_bounds[1] < x+1):
+                    if plot[y][x+1] == 1:
                         step = 0
-                if not (y_lim[0] > x -1  or y_lim[1] < x - 1):
-                    if plot[y][x - 1] != 0:
+                if not (x_bounds[0] > x-1 or x_bounds[1] < x-1):
+                    if plot[y][x-1] == 1:
                         step = 0
-                if not (x_lim[0] > y + 1 or x_lim[1] < y + 1):
-                    if plot[y + 1][x] != 0:
+                if not (y_bounds[0] > y+1 or y_bounds[1] < y+1):
+                    if plot[y+1][x] == 1:
                         step = 0
-                if not (x_lim[0] > y - 1 or x_lim[1] < y - 1):
-                    if plot[y - 1][x] != 0:
+                if not (y_bounds[0] > y-1 or y_bounds[1] < y-1):
+                    if plot[y-1][x] == 1:
                         step = 0
-                if step == 1:
+                if step:
                     count += 1
                     plot[y][x] = 1
     return count
