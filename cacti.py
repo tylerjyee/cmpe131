feefa9806c0361f64a1bcb2dbb40d23b69289a96
tylerjyee <tyler.j.yee@sjsu.edu>
2023-03-17 15:13:52 -0700

    changes to cacti program

--- cacti.py
+++ cacti.py
@@ -8,19 +8,19 @@ def cacti_number(plot):
 
             if plot[y][x] == 0:
                 step = 1
-                if not (x_lim[0] > x + 1 or x_lim[1] < x + 1):
+                if not (y_lim[0] > x + 1 or y_lim[1] < x + 1):
                     if plot[y][x + 1] != 0:
                         step = 0
-                if not (x_lim[0] > x -1  or x_lim[1] < x - 1):
+                if not (y_lim[0] > x -1  or y_lim[1] < x - 1):
                     if plot[y][x - 1] != 0:
                         step = 0
-                if not (y_lim[0] > y + 1 or y_lim[1] < y + 1):
+                if not (x_lim[0] > y + 1 or x_lim[1] < y + 1):
                     if plot[y + 1][x] != 0:
                         step = 0
-                if not (y_lim[0] > y - 1 or y_lim[1] < y - 1):
+                if not (x_lim[0] > y - 1 or x_lim[1] < y - 1):
                     if plot[y - 1][x] != 0:
                         step = 0
                 if step == 1:
                     count += 1
-                    plot[x][y] = 1
+                    plot[y][x] = 1
     return count
