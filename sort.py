3192d32331b5ce3de530b0072545e9d40d1211a6
tylerjyee <tyler.j.yee@sjsu.edu>
2023-03-17 15:01:56 -0700

    minor changes to sort dictionary function

--- sort.py
+++ sort.py
@@ -1,7 +1,6 @@
-empty_list = []
 def sort_dictionary(dictionary):
+    empty_list = []
     age_sort= sorted(dictionary.items(), key=lambda x: x[1][1])
-    
     for name, value in age_sort:
         empty_list.append((name, value[0]))
     return empty_list
