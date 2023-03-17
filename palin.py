1c8a34b4a0357ebe8fa30459863491c230271d0b
tylerjyee <tyler.j.yee@sjsu.edu>
2023-03-17 08:33:48 -0700

    modified palin.py further

--- palin.py
+++ palin.py
@@ -1,4 +1,4 @@
-def palindrome(test_list):
-    rev_list = list(reversed(test_list))
-    res = rev_list == test_list
+def palindrome(in_list):
+    new_list = list(reversed(in_list))
+    res = new_list == in_list
     return res
